35dc92e01d68dac783cebe2fd00b3002fcacb518
kpeezydev <50434474+kpeezydev@users.noreply.github.com>
2019-05-09 20:09:24 +1200

    Update CallDriver.java

--- src/test/java/Functions/CallDriver.java
+++ src/test/java/Functions/CallDriver.java
@@ -8,14 +8,14 @@ public class CallDriver {
     public static WebDriver driver;
 
     public static WebDriver chrome(){//(String url){
-        //String path = System.getProperty("user.dir");
+        String path = System.getProperty("user.dir");
  
      //  System.setProperty("webdriver.chrome.driver",path+"/tree/master/drivers/chromedriver.exe" );
-        System.out.print("BLASABDSKLDSKLAKDHLSAKHLDKHLSAKHL");
-        System.out.println(Paths.get(""));
+        //System.out.print("BLASABDSKLDSKLAKDHLSAKHLDKHLSAKHL");
+        //System.out.println(Paths.get(""));
         
         
-        System.setProperty("webdriver.chrome.driver","/drivers/chromedriver.exe" );
+        System.setProperty("webdriver.chrome.driver",path+"/drivers/chromedriver.exe" );
 //		ChromeOptions chromeOptions = new ChromeOptions();
 //		chromeOptions.setHeadless(true);
         driver = new ChromeDriver();
