a4235d358c05915d22eae88ebc82f8342d1d45f6
kpeezydev <50434474+kpeezydev@users.noreply.github.com>
2019-05-09 20:30:07 +1200

    Update CallDriver.java

--- src/test/java/Functions/CallDriver.java
+++ src/test/java/Functions/CallDriver.java
@@ -14,8 +14,8 @@ public class CallDriver {
         System.out.print("BLASABDSKLDSKLAKDHLSAKHLDKHLSAKHL");
         //System.out.println(Paths.get(""));
         
-        
-        System.setProperty("webdriver.chrome.driver",path+"/drivers/chromedriver.exe" );
+        System.setProperty("webdriver.chrome.driver","https://raw.githubusercontent.com/codeship/scripts/master/packages/chromedriver.sh");
+//        System.setProperty("webdriver.chrome.driver",path+"/drivers/chromedriver.exe" );
 //		ChromeOptions chromeOptions = new ChromeOptions();
 //		chromeOptions.setHeadless(true);
         driver = new ChromeDriver();
