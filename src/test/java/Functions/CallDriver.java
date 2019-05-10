884d46e5766b3dbed82593085aade5b45806a1d3
kpeezydev <50434474+kpeezydev@users.noreply.github.com>
2019-05-10 15:40:06 +1200

    Update CallDriver.java

--- src/test/java/Functions/CallDriver.java
+++ src/test/java/Functions/CallDriver.java
@@ -8,26 +8,15 @@ import java.io.File;
 public class CallDriver {
 
     public static WebDriver driver;
-    private static String systemOS = System.getProperty("os.name").toLowerCase();
-    private static String path = System.getProperty("user.dir");
 
     public static WebDriver chrome(){//(String url){
+        String path = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",path+"/drivers/chromedriver-linux" );
 //		ChromeOptions chromeOptions = new ChromeOptions();
 //		chromeOptions.setHeadless(true);
-        setChromedriverInstance();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         return driver;
-    }
-
-    private static void setChromedriverInstance() {
-        if (systemOS.contains("windows")) {
-            System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver-windows.exe");
-        } else if (systemOS.contains("mac")) {
-            System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver-mac");
-        } else {
-            System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver-linux");
-        }
     }
 
 } 
