e920b6704c76c6f323471819b131adb4b7daa192
kpeezydev <50434474+kpeezydev@users.noreply.github.com>
2019-05-10 13:10:52 +1200

    Update CallDriver.java

--- src/test/java/Functions/CallDriver.java
+++ src/test/java/Functions/CallDriver.java
@@ -9,6 +9,7 @@ public class CallDriver {
 
     public static WebDriver chrome(){//(String url){
         String path = System.getProperty("user.dir");
+     
 
         
         //System.setProperty("webdriver.chrome.driver","chromedriver.sh");
